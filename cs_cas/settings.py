d37df2fe43c8a55de25b372fac1b3733479d43be
areebimran2 <areeb.imran@mail.utoronto.ca>
2025-09-13 07:08:58 -0400

    Switch to psycopg3 and Python version 3.12.11

--- cs_cas/settings.py
+++ cs_cas/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'cs_cas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'cscasadmin',
