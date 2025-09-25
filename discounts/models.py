ae3caf600888bd3d2b52f1e2c0d311eb1124648e
areebimran2 <areeb.imran@mail.utoronto.ca>
2025-09-25 09:37:17 -0400

    Completed DDL and revisions for Discounts module

--- discounts/models.py
+++ discounts/models.py
@@ -1,65 +1,81 @@
 from django.db import models
-from django.db.models import ExpressionWrapper
-from django.db.models.functions import Now
 from django.contrib.postgres.functions import RandomUUID
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
+from django.db.models.functions import Coalesce
+
+from common.enums import DiscountKind, SalesChannel, DiscountStatus
+from common.fields import PostgresEnumField
+from common.functions import TxNow
 
 class Discount(models.Model):
-    id = models.UUIDField(primary_key=True, default=RandomUUID, db_default=RandomUUID())
+    id = models.UUIDField(primary_key=True, db_default=RandomUUID())
     name = models.TextField(null=False)
-
-    class Kind(models.TextChoices):
-        PERCENT = 'percent', _('Percent')
-        FIXED = 'fixed', _('Fixed')
-
-    kind = models.CharField(max_length=7, choices=Kind.choices, null=False)
+    kind = PostgresEnumField('discount_kind', choices=DiscountKind.choices, null=False)
     value = models.DecimalField(max_digits=10, decimal_places=4, null=False)
-
-    class Channel(models.TextChoices):
-        B2B = 'b2b', _('B2B')
-        B2C = 'b2c', _('B2C')
-        BOTH = 'both', _('Both')
-
-    channel = models.CharField(max_length=4, default=Channel.B2B, db_default=Channel.B2B,
-                               choices=Channel.choices, null=False)
+    channel = PostgresEnumField('sales_channel', db_default=SalesChannel.BOTH, choices=SalesChannel.choices, null=False)
     starts_at = models.DateTimeField(null=False)
     ends_at = models.DateTimeField(null=False)
-    min_margin_b2b = models.DecimalField(max_digits=6, decimal_places=4, default=0.05, db_default=0.05, null=False)
-    min_margin_b2c = models.DecimalField(max_digits=6, decimal_places=4, default=0.10, db_default=0.10, null=False)
-
-    class Status(models.TextChoices):
-        SCHEDULED = "scheduled", _("Scheduled")
-        ACTIVE = "active", _("Active")
-        ENDED = "ended", _("Ended")
-        CANCELLED = "cancelled", _("Cancelled")
-
-    status = models.CharField(max_length=9, default=Status.SCHEDULED, db_default=Status.SCHEDULED,
-                              choices=Status.choices, null=False)
+    min_margin_b2b = models.DecimalField(max_digits=6, decimal_places=4, db_default=0.05, null=False)
+    min_margin_b2c = models.DecimalField(max_digits=6, decimal_places=4, db_default=0.10, null=False)
+    status = PostgresEnumField('discount_status', db_default=DiscountStatus.SCHEDULED, choices=DiscountStatus.choices, null=False)
     # created_by = models.ForeignKey('myadmin.User')
-    created_at = models.DateTimeField(default=timezone.now, db_default=Now(), null=False)
+    created_at = models.DateTimeField(db_default=TxNow(), null=False)
+
+    class Meta:
+        db_table = 'discounts'
 
 class DiscountTarget(models.Model):
-    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, null=False)
-    target_kind =  models.TextField(null=False)
-    sailing = models.ForeignKey('seasons_sailings.Sailing', on_delete=models.CASCADE)
-    category = models.ForeignKey('catalogs.CabinCategory', on_delete=models.CASCADE)
-    cabin = models.ForeignKey('ships_cabins.Cabin', on_delete=models.CASCADE)
-    pk = models.CompositePrimaryKey("discount_id", "target_kind")
+    # Since composite primary keys don't support generated columns, add a surrogate primary key with a unique
+    # constraint for now.
+    id = models.UUIDField(primary_key=True, db_default=RandomUUID())
 
-    # The below is invalid DDL and also breaks referential integrity in the database. Nullable fields cannot be part of a primary key.
-    # pk = models.CompositePrimaryKey("discount_id", "target_kind", COALESCE("sailing_id", RANDOM_UUID()))
+    TARGET_KIND_CHOICES = (
+        ('sailing', 'sailing'),
+        ('category', 'category'),
+        ('cabin', 'cabin'),
+    )
+
+    target_kind =  models.TextField(choices=TARGET_KIND_CHOICES, null=False)
+
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, null=False, db_index=False)
+    sailing = models.ForeignKey('seasons_sailings.Sailing', on_delete=models.CASCADE, null=True, db_index=False)
+    category = models.ForeignKey('catalogs.CabinCategory', on_delete=models.CASCADE, null=True, db_index=False)
+    cabin = models.ForeignKey('ships_cabins.Cabin', on_delete=models.CASCADE, null=True, db_index=False)
+
+    # Use a generated column to use in the composite primary key as one of sailing_id, category_id, cabin_id
+    # must be non-null, therefore this column will always be non-null and is permitted in a primary key.
+    # cabin > category > sailing
+    target_id = models.GeneratedField(
+        expression=Coalesce(models.F('cabin_id'), models.F('category_id'), models.F('sailing_id')),
+        output_field=models.UUIDField(),
+        db_persist=True,
+        null=False,
+    )
+
+    # The below is unsupported by Django 5.2.6, instead enforce uniqueness with a unique constraint.
+    # pk = models.CompositePrimaryKey('discount_id', 'target_kind', 'target_id')
 
 
     class Meta:
+        db_table = 'discount_targets'
         constraints = [
-            # This is logically equivalent to:
-            # ((sailing_id IS NOT NULL)::int + (category_id IS NOT NULL)::int + (cabin_id IS NOT NULL)::int) = 1
             models.CheckConstraint(
-                check=(models.Q(sailing__isnull=False, category__isnull=True, cabin__isnull=True) |
-                       models.Q(sailing__isnull=True, category__isnull=False, cabin__isnull=True) |
-                       models.Q(sailing__isnull=True, category__isnull=True, cabin__isnull=False)),
-                name='ck_target_exactly_one'
-            )
+                check=models.Q(target_kind__in=['sailing','category','cabin']),
+                name='discount_targets_target_kind_check',
+            ),
+            models.CheckConstraint(
+                check=(
+                    (models.Q(target_kind='sailing') &
+                     models.Q(sailing__isnull=False, category__isnull=True, cabin__isnull=True)) |
+                    (models.Q(target_kind='category') &
+                     models.Q(sailing__isnull=True, category__isnull=False, cabin__isnull=True)) |
+                    (models.Q(target_kind='cabin') &
+                     models.Q(sailing__isnull=True, category__isnull=True, cabin__isnull=False))
+                ),
+                name='ck_target_exactly_one',
+            ),
+            models.UniqueConstraint(
+                fields=['discount', 'target_kind', 'target_id'],
+                name='discount_targets_discount_id_target_kind_target_id_key',
+            ),
         ]
 
